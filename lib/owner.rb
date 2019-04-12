01ae363ca2128dc4f9d12d87d739579bc3e53d75
David Stewart <davidstewart@gmail.com>
2019-04-12 01:48:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,9 @@ def sell_pets
       animals.clear
     end
 end   
-  
+    def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
   
   
 end
